c281cc0852d76e020b376339ac8b895eebf8e232
SIVARANJANI R <sivasumi1523@gmail.com>
2025-11-26 23:14:05 +0530

    Update UserService.java

--- src/main/java/com/example/event/service/UserService.java
+++ src/main/java/com/example/event/service/UserService.java
@@ -22,7 +22,7 @@ public class UserService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(encoder.encode(password));
-        user.setRole("ADMIN");
+        user.setRole("USER");
         return userRepo.save(user);
     }
 
